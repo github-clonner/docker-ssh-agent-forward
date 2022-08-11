f8e55898536217182b96a38672cf1ab04325faf9
Patrick Luzolo <eldorplus@gmail.com>
2022-08-11 17:23:37 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -eo pipefail
 
+# enable debug mode if desired
+if [[ "${DEBUG}" == "true" ]]; then 
+    set -x
+fi
+
 echo "$AUTHORIZED_KEYS" | base64 -d >/root/.ssh/authorized_keys
 
 chown root:root /root/.ssh/authorized_keys
